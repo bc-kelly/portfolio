c7d7503eb2711d4bb6f5e0c159a9ca26bf199ca8
Bridget Kelly <bckelly197@gmail.com>
2022-06-08 16:56:10 -0400

    routes are working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
-// import NavBar from "./NavBar"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar"
 import Home from "./Home"
 import Projects from "./Projects"
 import Contact from "./Contact"
@@ -9,17 +10,29 @@ import Contact from "./Contact"
 function App() {
 
   return (
-    <div className="App">
-      <h1> hello from app </h1> 
-      <h2> welcome to my portfolio </h2>
-      <Home />
-      <Projects />
-      <Contact />
-      {/* <NavBar /> */}
-        {/* <Routes>
-          <Route path ="/home" element= { <Home /> } /> 
-        </Routes> */}
-    </div>
+    <Router> 
+      <div className="App">
+        <h1> hello from app </h1> 
+        <h2> welcome to my portfolio </h2>
+        {/* <Home />
+        <Projects />
+        <Contact /> */}
+        <NavBar />
+          <div className="content"> 
+            <Switch>
+              <Route path ="/home" > 
+                <Home />
+              </Route>
+              <Route path ="/contact" > 
+                <Contact />
+              </Route>
+              <Route path ="/projects" > 
+                <Projects />
+              </Route>
+            </Switch>
+          </div>
+      </div>
+    </Router>
   );
 }
 

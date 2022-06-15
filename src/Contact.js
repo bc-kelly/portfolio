af5b97b5dfab8aea9fdcf76de3fc8652d49771f2
Bridget Kelly <bckelly197@gmail.com>
2022-06-15 14:42:47 -0400

    added links

--- src/Contact.js
+++ src/Contact.js
@@ -5,8 +5,12 @@ function Contact() {
   return (
     <div className="contact-div">
       
-      <h3>Please feel free to contact me at </h3>
-      <a href="https://www.linkedin.com/in/bridget-kelly-594009127/" > LinkedIn </a>
+      <h3>Please feel free to contact me at: </h3>
+      <div> 
+        <a className="links" href="https://www.linkedin.com/in/bridget-kelly-594009127/" > LinkedIn </a>
+        <a className="links" href="https://github.com/bc-kelly" > GitHub </a>
+        <a className="links" href="https://dev.to/bc_kelly" > Dev Blog </a>
+      </div>
       <h4> (516) 668-0424 </h4>
       <h4> bckelly197@gmail.com </h4>
 

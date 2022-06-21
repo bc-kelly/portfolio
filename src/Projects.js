fe2164da89cea4afd46c82a68277e734e0bda50d
Bridget Kelly <bckelly197@gmail.com>
2022-06-20 23:59:04 -0400

    starting on projects

--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,20 @@ import './Projects.css';
 function Projects() {
   return (
     <div className = "projects-div">
-      hello from projects
+      <div> 
+        <div className = "project-title">Vacation Pro </div>
+        <div className="details"> 
+          description
+          link to project
+          demo
+        </div>
+      </div>
+      <div> 
+        Star Wars: Battle of the Known Universe
+      </div>
+      <div> 
+        Retro BnB
+      </div>
     </div>
   );
 }

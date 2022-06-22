f8927392a2c7e3b375179f1695e0b4f385688f1d
Bridget Kelly <bckelly197@gmail.com>
2022-06-22 14:35:35 -0400

    projects added

--- src/Projects.js
+++ src/Projects.js
@@ -9,28 +9,37 @@ function Projects() {
         <div className="details"> 
           <h4 className="description"> Vacation Pro is a group travel planning website to help users plan trips with their family and friends, designed to keep everyone in the loop. Users have the ability to create an account, add multiple profiles to their account, browse hotels, plan and save trips, and invite friends to join their account.
           </h4>
-          <h5 className="description-two"> Used JSON Web Tokens and localStorage to store encrypted account and profile information client-side, as well as sessions to store saved trips to individual profiles.
+          <h5 className="description-two"> Built using JavaScript, React, Ruby, and Ruby on Rails. Deployed on Heroku. Used JSON Web Tokens and localStorage to store encrypted account and profile information client-side, as well as sessions to store saved trips to individual profiles.
           Implemented an “Invite a friend” button and used EmailJS to send out an invitation containing a link to the site.
-          Hotel data stored using postgres database, mnaually seeded.  
+          Hotel data stored using postgres database, mnaually seeded. 
+          Deployed using Heroku. 
           </h5>
           <a className="website-link" href="https://vacation-pro.herokuapp.com/"> Website Link </a>
           <a className="website-link" href="https://www.loom.com/share/732285ba255a44a28f6215824b67173c"> Demo Video </a>
+          <a className="website-link" href="https://github.com/bc-kelly/my-vacay-pro"> GitHub Repository </a>
         </div>
       </div>
       <div className="indiv-proj"> 
         <h1 className = "project-title"> Star Wars: Battle of the Known Universe </h1>
         <div className="details"> 
-          <h4> This project</h4>
-          link to project
-          demo
+          <h4 className="description"> This website is a Star Wars themed game of Rock, Paper, Scissors. Users have the ability to create an account and log in, view all of the listed characters and planets, add their own character, and choose characters to participate in their battle. </h4>
+          <h5 className="description-two"> Built using JavaScript, React, Ruby, and Ruby on Rails. Incorporated various CSS techniques in order to recreate several iconic Star Wars features, such as the scrolling intro and the logo rotating and shooting into space.
+          Included validations in Rails to ensure users create a full profile and new characters have all necessary attributes.
+          </h5>
+          <a className="website-link" href="https://www.loom.com/share/3112d27983ef43c3b5e341d1cf303e0b"> Demo Video </a>
+          <a className="website-link" href="https://github.com/bc-kelly/phase-4-project-starwarsbattle"> GitHub Repository </a>
         </div>
       </div>
       <div className="indiv-proj"> 
         <h1 className = "project-title"> Retro BnB</h1>
         <div className="details"> 
-          description
-          link to project
-          demo
+          <h4 className="description"> Retro BnB is a vacation home rental website. Users have the ability to browse luxury vacation homes offered on the site, as well as add new homes, and leave and edit reviews and ratings.
+          </h4>
+          <h5 className="description-two"> Built using Javascript, React.js, and Ruby. Frontend deployed on Netlify, backend deployed on Heroku. Utilized Active Record Migrations and seed data to seed the database, containing vacation home images and locations. 
+          Incorporated CSS animations to allow for a more interactive user experience. 
+          </h5>
+          <a className="website-link" href="https://hotelproject-react.netlify.app/"> Website Link </a>
+          <a className="website-link" href="https://github.com/bc-kelly/phase-3-project-front-end"> GitHub Repository</a>
         </div>
       </div>
     </div>

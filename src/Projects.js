c7d08cef573e9e9e35a8c495c9f3b4d09b1cce49
Bridget Kelly <bckelly197@gmail.com>
2022-06-22 14:16:37 -0400

    adjust font, add projet details

--- src/Projects.js
+++ src/Projects.js
@@ -5,23 +5,24 @@ function Projects() {
   return (
     <div className = "projects-div">
       <div className="indiv-proj"> 
-        <div className = "project-title">Vacation Pro </div>
+        <h1 className = "project-title">Vacation Pro </h1>
         <div className="details"> 
-          description
+          <h4> Vacation Pro is a group travel planning website to help users plan trips with their family and friends, designed to keep everyone in the loop. Users have the ability to create an account, add multiple profiles to their account, browse hotels, plan and save trips, and invite friends to join their account.
+          </h4>
           link to project
           demo
         </div>
       </div>
       <div className="indiv-proj"> 
-        <div className = "project-title"> Star Wars: Battle of the Known Universe</div>
+        <h1 className = "project-title"> Star Wars: Battle of the Known Universe </h1>
         <div className="details"> 
-          description
+          <h4> This project</h4>
           link to project
           demo
         </div>
       </div>
       <div className="indiv-proj"> 
-        <div className = "project-title"> Retro BnB</div>
+        <h1 className = "project-title"> Retro BnB</h1>
         <div className="details"> 
           description
           link to project

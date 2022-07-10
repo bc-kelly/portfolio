1cd64a2cb626874361f20753a23a39b0c738d47e
Bridget Kelly <bckelly197@gmail.com>
2022-07-10 15:21:25 -0400

    more about

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <div className="about-div">
         <h4> 
-            Hi, my name is Bridget Kelly and I am a Software Engineer. I am a recent gradute of the Flatiron School in NYC, where I completed the Full Stack Software Engineering program. Prior to that I worked as a Financial Analyst for IBM and Kyndryl. I graduated from Marist College in May of 2019 with my Bachelor of Science in Business Administration with a concentration in Finance and a minor in Accounting. Please feel free to check out my blog and GitHub to see what I am working on! Links are on the Contact page. 
+            Hi, my name is Bridget Kelly and I am a Software Engineer. I have a strong interest in the front end and I am currently working to broaden my CSS skills. I am a recent gradute of the Flatiron School in NYC, where I completed the Full Stack Software Engineering program. Prior to that I worked as a Financial Analyst for IBM and Kyndryl. I graduated from Marist College in May of 2019 with my Bachelor of Science in Business Administration with a concentration in Finance and a minor in Accounting. Please feel free to check out my blog and GitHub to see what I am working on! Links are on the Contact page. 
         </h4>
     </div>
   );
